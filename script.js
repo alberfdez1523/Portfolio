7e904eb1c098b5e4d3a2a98687844e0246fd5a04
alberfdez1523 <alberfdez2000@gmail.com>
2025-01-09 17:41:21 +0100

    Subir portfolio

---
+++ script.js
@@ -0,0 +1,55 @@
+let currentLanguage = 'en';
+
+function changeLanguage() {
+    if (currentLanguage === 'en') {
+        console.log('Cambiando idioma:', currentLanguage);
+        document.getElementById('greeting').innerHTML = 'Hola <span class="animate-wave">👋</span>, soy';
+        document.getElementById('profession').textContent = 'INGENIERO DE SOFTWARE';
+        document.getElementById('scroll').innerHTML = 'Desplázate <span>&#x2193;</span>';
+        document.getElementById('about-title').textContent = 'SOBRE MÍ';
+        document.getElementById('know-more').textContent = 'Saber Más';
+        document.getElementById('language-button').textContent = 'ENG';
+        document.getElementById('experience-title').textContent = 'EXPERIENCIA';
+        document.getElementById('nav-about').textContent = '<sobre mí/>';
+        document.getElementById('nav-experience').textContent = '<experiencia/>';
+        document.getElementById('nav-intro').textContent = '<inicio/>';
+        document.getElementById('link_experience').textContent = 'Ver más experiencia';
+        document.getElementById('link_education').textContent = 'Ver mi educación';
+        document.getElementById('job1_title').textContent = 'Operador de Bases de Datos';
+        document.getElementById('job1_period').textContent = 'Mayo 2024 - Julio 2024';
+        document.getElementById('job1_description').textContent = 'Alfotrailer, S.L. / Asistencia en la resolución de problemas relacionados con las bases de datos e implementación de soluciones. Actualización y modificación de los registros en la base de datos.';
+        document.getElementById('link_cv').textContent = 'Ver Currículum Vitae';
+        document.getElementById('about-paragraph-1').textContent = 'Desde siempre me he sentido atraído por la informática, pero no fue hasta que empecé a profundizar en la programación que descubrí que este era realmente mi lugar, el espacio donde quiero seguir creciendo y aprendiendo.';
+        document.getElementById('about-paragraph-2').textContent = 'Las primeras etapas en la universidad no fueron fáciles para mí. Al principio, no sabía muy bien cómo abordar los estudios de Ingeniería del Software ni cómo encajar en este mundo tan amplio y complejo. Sin embargo, con el tiempo, llegó un momento clave en el que mi perspectiva cambió: aprendí a pensar como un ingeniero, centrándome en la resolución de problemas y en la estructura del pensamiento más que en las habilidades mismas de programación. Este enfoque es algo que considero una de mis mayores fortalezas.';
+        document.getElementById('about-paragraph-3').textContent = 'Me apasiona aprender nuevos lenguajes de programación y enfrentarme a retos. Aunque no domine un lenguaje en particular al principio, tengo la capacidad de adaptarme rápidamente y absorber conocimientos como una esponja. Esta misma adaptabilidad me permite integrarme con facilidad tanto en equipos como en entornos prácticos.';
+        document.getElementById('about-paragraph-4').textContent = 'En mi tiempo libre, disfruto de mis tres grandes aficiones: los videojuegos, el baloncesto y el cine. Estas pasiones no solo me inspiran, sino que también me ayudan a desconectar y mantener el equilibrio en mi día a día. Estoy convencido de que siempre hay algo nuevo que aprender y aportar, y me entusiasma enfrentar cualquier reto que me ayude a seguir creciendo como profesional y como persona.';
+        currentLanguage = 'es';
+    } else {
+        console.log('Cambiando idioma:', currentLanguage);
+        document.getElementById('greeting').innerHTML = 'Hi <span class="animate-wave">👋</span>, I\'m';
+        document.getElementById('profession').textContent = 'SOFTWARE ENGINEER';
+        document.getElementById('scroll').innerHTML = 'Scroll <span>&#x2193;</span>';
+        document.getElementById('about-title').textContent = 'ABOUT';
+        document.getElementById('know-more').textContent = 'Know More';
+        document.getElementById('language-button').textContent = 'ESP';
+        document.getElementById('experience-title').textContent = 'EXPERIENCE';
+        document.getElementById('nav-about').textContent = '<about/>';
+        document.getElementById('nav-experience').textContent = '<experience/>';
+        document.getElementById('nav-intro').textContent = '<intro/>';
+        document.getElementById('link_experience').textContent = 'View more experience';
+        document.getElementById('link_education').textContent = 'View my education';
+        document.getElementById('job1_title').textContent = 'Database Operator';
+        document.getElementById('job1_period').textContent = 'May 2024 - July 2024';
+        document.getElementById('job1_description').textContent = 'Alfotrailer, S.L. / Assistance in solving problems related to databases and implementing solutions. Updating and modifying records in the database.';
+        document.getElementById('link_cv').textContent = 'View Curriculum Vitae';
+        document.getElementById('about-paragraph-1').textContent = 'I have always been drawn to computer science, but it wasn\'t until I delved deeper into programming that I realized this was truly my place—the field where I want to keep growing and learning.';
+        document.getElementById('about-paragraph-2').textContent = 'The early stages of university were not easy for me. At first, I wasn’t quite sure how to approach Software Engineering studies or how to find my footing in such a broad and complex field. However, over time, there was a pivotal moment when my perspective shifted: I learned to think like an engineer, focusing on problem-solving and structured thinking rather than just programming skills. This mindset is something I consider one of my greatest strengths.';
+        document.getElementById('about-paragraph-3').textContent = 'I am passionate about learning new programming languages and tackling challenges. Even if I don’t initially master a particular language, I have the ability to adapt quickly and absorb knowledge like a sponge. This adaptability also enables me to integrate seamlessly into both teams and practical environments.';
+        document.getElementById('about-paragraph-4').textContent = 'In my free time, I enjoy my three main hobbies: video games, basketball, and cinema. These passions not only inspire me but also help me disconnect and maintain balance in my daily life. I firmly believe there is always something new to learn and contribute, and I’m excited to face any challenge that helps me continue growing both as a professional and as a person.';
+        currentLanguage = 'en';
+    }
+}
+
+function toggleTheme() {
+    document.body.classList.toggle('light-mode');
+}
